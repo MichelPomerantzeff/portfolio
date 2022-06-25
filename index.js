fd2740a86213961876cf92f440a9ce3dbaba65e1
Michel Pomerantzeff <micheldauniao@hotmail.com>
2022-06-25 21:57:44 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,87 @@
+// Scroll to About section
+const topbarAbout = document.querySelector(".topbarAbout")
+const aboutBtn = document.querySelector(".toAbout")
+const aboutBox = document.querySelector(".aboutBox")
+
+aboutBtn.addEventListener("click", () => {
+    aboutBox.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+topbarAbout.addEventListener("click", () => {
+    aboutBox.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+
+
+// Scroll to Skilss section
+const topbarSkill = document.querySelector(".topbarSkill")
+const btnSkill = document.querySelector(".toSkill")
+const skillsBox = document.querySelector(".skillsBox")
+
+btnSkill.addEventListener("click", () => {
+    skillsBox.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+topbarSkill.addEventListener("click", () => {
+    skillsBox.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+
+
+// Scroll to Projects section
+const topbarProject = document.querySelector(".topbarProject")
+const btnProject = document.querySelector(".toProject")
+const projectContainer = document.querySelector(".projectContainer")
+
+btnProject.addEventListener("click", () => {
+    projectContainer.scrollIntoView({ behavior: "smooth", block: "start" })
+})
+topbarProject.addEventListener("click", () => {
+    projectContainer.scrollIntoView({ behavior: "smooth", block: "start" })
+})
+
+
+// Scroll to Contact section
+const topbarContact = document.querySelector(".topbarContact")
+const btnContact = document.querySelector(".toContact")
+const contact = document.querySelector(".contact")
+
+btnContact.addEventListener("click", () => {
+    contact.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+topbarContact.addEventListener("click", () => {
+    contact.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+
+
+const homeBtn = document.querySelector('#homeBtn')
+const house = document.querySelector('.house')
+const initials = document.querySelector('.initials')
+
+
+
+homeBtn.addEventListener("mouseenter", () => {
+    initials.classList.add("hide")
+    house.classList.add("fa-house")
+    
+})
+
+homeBtn.addEventListener("mouseleave", () => {
+    house.classList.remove("fa-house")
+    initials.classList.remove("hide")
+})
+
+
+const home = document.querySelector(".home")
+const upArrow = document.querySelector(".upArrow")
+
+
+upArrow.addEventListener("click", () => {
+    home.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+homeBtn.addEventListener("click", () => {
+    home.scrollIntoView({ behavior: "smooth", block: "center" })
+})
+
+const footer = document.querySelector("footer")
+const down = document.querySelector(".down")
+
+down.addEventListener("click", () => {
+    footer.scrollIntoView({ behavior: "smooth", block: "center" })
+})
